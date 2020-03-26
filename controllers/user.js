62105a5f19820115b24c0723280456f03819a4c1
yuanhan2019 <yuanh143@gmail.com>
2020-03-26 23:02:40 +0000

    Architecture

--- controllers/user.js
+++ controllers/user.js
@@ -1,54 +1,54 @@
-// var Character = require('../models/user');
-// // exports.getAge = function (req, res) {
-// //     var userData = req.body;
-// //     if (userData == null) {
-// //         res.status(403).send('No data sent!')
-// //     }
-// //     try {
-// //         Character.find({first_name: userData.firstname, family_name: userData.lastname},
-// //             'first_name family_name dob age',
-// //             function (err, characters) {
-// //                 if (err)
-// //                     res.status(500).send('Invalid data!');
-// //                 var character = null;
-// //                 if (characters.length > 0) {
-// //                     var firstElem = characters[0];
-// //                     character = {
-// //                         name: firstElem.first_name, surname: firstElem.family_name,
-// //                         dob: firstElem.dob, age: firstElem.age
-// //                     };
-// //                 }
-// //                 res.setHeader('Content-Type', 'application/json');
-// //                 res.send(JSON.stringify(character));
-// //             });
-// //     } catch (e) {
-// //         res.status(500).send('error ' + e);
-// //     }
-// // }
-// //
-// //
-// // exports.insert = function (req, res) {
-// //     var userData = req.body;
-// //     if (userData == null) {
-// //         res.status(403).send('No data sent!')
-// //     }
-// //     try {
-// //         var character = new Character({
-// //             first_name: userData.firstname,
-// //             family_name: userData.lastname,
-// //             dob: userData.year
-// //         });
-// //         console.log('received: ' + character);
-// //
-// //         character.save(function (err, results) {
-// //             console.log(results._id);
-// //             if (err)
-// //                 res.status(500).send('Invalid data!');
-// //
-// //             res.setHeader('Content-Type', 'application/json');
-// //             res.send(JSON.stringify(character));
-// //         });
-// //     } catch (e) {
-// //         res.status(500).send('error ' + e);
-// //     }
-// // }
+var User = require('../models/user');
+exports.getAge = function (req, res) {
+    var userData = req.body;
+    if (userData == null) {
+        res.status(403).send('No data sent!')
+    }
+    try {
+        Character.find({first_name: userData.firstname, family_name: userData.lastname},
+            'first_name family_name dob age',
+            function (err, characters) {
+                if (err)
+                    res.status(500).send('Invalid data!');
+                var character = null;
+                if (characters.length > 0) {
+                    var firstElem = characters[0];
+                    character = {
+                        name: firstElem.first_name, surname: firstElem.family_name,
+                        dob: firstElem.dob, age: firstElem.age
+                    };
+                }
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify(character));
+            });
+    } catch (e) {
+        res.status(500).send('error ' + e);
+    }
+}
+
+
+exports.insert = function (req, res) {
+    var userData = req.body;
+    if (userData == null) {
+        res.status(403).send('No data sent!')
+    }
+    try {
+        var character = new Character({
+            first_name: userData.firstname,
+            family_name: userData.lastname,
+            dob: userData.year
+        });
+        console.log('received: ' + character);
+
+        character.save(function (err, results) {
+            console.log(results._id);
+            if (err)
+                res.status(500).send('Invalid data!');
+
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify(character));
+        });
+    } catch (e) {
+        res.status(500).send('error ' + e);
+    }
+}
--- models/user.js
+++ models/user.js
@@ -4,20 +4,20 @@ var Schema = mongoose.Schema;
 
 var User = new Schema(
     {
-        // first_name: {type: String, required: true, max: 100},
-        // family_name: {type: String, required: true, max: 100},
+         username: {type: String, required: true, max: 100},
+         password: {type: String, required: true, max: 100},
         // dob: {type: Number},
         // whatever: {type: String} //any other field
     }
 );
 
-// Virtual for a character's age
-user.virtual('age')
-    .get(function () {
-        const currentDate = new Date().getFullYear();
-        const result= currentDate - this.dob;
-        return result;
-    });
+// // Virtual for a character's age
+// user.virtual('age')
+//     .get(function () {
+//         const currentDate = new Date().getFullYear();
+//         const result= currentDate - this.dob;
+//         return result;
+//     });
 
 User.set('toObject', {getters: true, virtuals: true});
 
