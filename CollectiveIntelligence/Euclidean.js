ed10ffd838922eeb2b736f51080738918627e2dd
acp19mz <55985990+acp19mz@users.noreply.github.com>
2020-05-28 21:23:50 +0100

    Revert "Add the Euclidean algorithm based on the file structure"
    This reverts commit 893e933129f0e5c91b871a080d7629c20224eb63.

--- CollectiveIntelligence/Euclidean.js
+++ CollectiveIntelligence/Euclidean.js
@@ -4,21 +4,13 @@ module.exports = class Euclidean {
 
     // Returns a distance-based similarity score for person1 and person2
     static sim(prefs, person1, person2) {
-        let ratings1;
-        let ratings2;
-        _.forIn(prefs, (value, key) => {
-            if(value['userId']==person1){
-                ratings1=value['ratings'];
-            }else if(value['userId']==person2){
-                ratings2=value['ratings'];
-            }
-        });
+
         // Get the list of shared_items
-        let person1Movies = _.map(ratings1, (n) => {
-            return _.values(n)[0];
+        let person1Movies = _.map(prefs[person1], (n) => {
+            return _.keys(n)[0];
         });
-        let person2Movies = _.map(ratings2, (n) => {
-            return _.values(n)[0];
+        let person2Movies = _.map(prefs[person2], (n) => {
+            return _.keys(n)[0];
         });
         let sharedMovies = _.intersection(person1Movies, person2Movies);
 
@@ -29,12 +21,13 @@ module.exports = class Euclidean {
         let sum = 0;
         _.forEach(sharedMovies, (value) => {
 
-            let person1Rating = _.values(_.find(ratings1, (e) => {
-                if (_.values(e)[0]==value) return _.values(e)[0];
-            }))[1];
-            let person2Rating = _.values(_.find(ratings2, (e) => {
-                if (_.values(e)[0]==value) return _.values(e)[0];
-            }))[1];
+            let person1Rating = _.values(_.find(prefs[person1], (e) => {
+                if (e[value]) return e[value];
+            }))[0];
+            let person2Rating = _.values(_.find(prefs[person2], (e) => {
+                if (e[value]) return e[value];
+            }))[0];
+
             let calc = Math.pow(person1Rating - person2Rating, 2);
             sum += calc;
         });
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser= require("body-parser");
-var fs = require('fs');
-var path = require('path');
+
 var initDB=require('../controllers/init');
 var setUser=require('../controllers/signup');
 var login=require('../controllers/login');
@@ -43,7 +42,8 @@ router.post('/insertStory',setStory.insertStory);
 router.post('/insertRating',setRating.insertRating);
 router.post('/ranking', function (req, res, next) {
   let name = req.body.ranking_name;
-  console.log("In post");
+  console.log("ranking_name");
+  console.log(name);
   let ranking= new Ranking();
   let results= ranking.getRecommendations(critics, name, 'sim_euclidean');
   res.setHeader('Content-Type', 'application/json');
@@ -51,85 +51,118 @@ router.post('/ranking', function (req, res, next) {
 });
 
 const critics = {
-
-  "users": [
-  {
-    "userId": "user_0",
-    "ratings": [
-      {
-        "storyId": "story_1",
-        "rating": 5
-      },
-      {
-        "storyId": "story_2",
-        "rating": 5
-      },
-      {
-        "storyId": "story_3",
-        "rating": 2
-      },
-      {
-        "storyId": "story_4",
-        "rating": 4
-      },
-      {
-        "storyId": "story_5",
-        "rating": 4
-      }
-      ]
-  },
-  {
-    "userId": "user_1",
-    "ratings": [
-      {
-        "storyId": "story_1",
-        "rating": 1
-      },
-      {
-        "storyId": "story_2",
-        "rating": 5
-      },
-      {
-        "storyId": "story_4",
-        "rating": 3
-      },
-      {
-        "storyId": "story_5",
-        "rating": 3
-      },
-      {
-        "storyId": "story_6",
-        "rating": 4
-      }
-    ]
+  'Lisa Rose': [{
+    'Lady in the Water': 2.5
   },
     {
-      "userId": "user_2",
-      "ratings": [
-        {
-          "storyId": "story_1",
-          "rating": 3
-        },
-        {
-          "storyId": "story_2",
-          "rating": 2
-        },
-        {
-          "storyId": "story_6",
-          "rating": 2
-        },
-        {
-          "storyId": "story_7",
-          "rating": 3
-        },
-        {
-          "storyId": "story_8",
-          "rating": 5
-        }
-      ]
+      'Snakes on a Plane': 3.5
+    },
+    {
+      'Just My Luck': 3.0
+    },
+    {
+      'Superman Returns': 3.5
+    },
+    {
+      'You, Me and Dupree': 2.5
+    },
+    {
+      'The Night Listener': 3.0
     }
+  ],
+  'Gene Seymour': [{
+    'Lady in the Water': 3.0
+  },
+    {
+      'Snakes on a Plane': 3.5
+    },
+    {
+      'Just My Luck': 1.5
+    },
+    {
+      'Superman Returns': 5.0
+    },
+    {
+      'You, Me and Dupree': 3.5
+    },
+    {
+      'The Night Listener': 3.0
+    }
+  ],
+  'Michael Phillips': [{
+    'Lady in the Water': 2.5
+  },
+    {
+      'Snakes on a Plane': 3.0
+    },
+    {
+      'Superman Returns': 3.5
+    },
+    {
+      'The Night Listener': 4.0
+    }
+  ],
+  'Claudia Puig': [{
+    'Snakes on a Plane': 3.5
+  },
+    {
+      'Just My Luck': 3.0
+    },
+    {
+      'Superman Returns': 4.0
+    },
+    {
+      'You, Me and Dupree': 2.5
+    },
+    {
+      'The Night Listener': 4.5
+    }
+  ],
+  'Mick LaSalle': [{
+    'Lady in the Water': 3.0
+  },
+    {
+      'Snakes on a Plane': 4.0
+    },
+    {
+      'Just My Luck': 2.0
+    },
+    {
+      'Superman Returns': 3.0
+    },
+    {
+      'You, Me and Dupree': 2.0
+    },
+    {
+      'The Night Listener': 3.0
+    }
+  ],
+  'Jack Matthews': [{
+    'Lady in the Water': 3.0
+  },
+    {
+      'Snakes on a Plane': 4.0
+    },
+    {
+      'Superman Returns': 5.0
+    },
+    {
+      'You, Me and Dupree': 3.5
+    },
+    {
+      'The Night Listener': 3.0
+    }
+  ],
+  'Toby': [{
+    'Snakes on a Plane': 4.5
+  },
+    {
+      'Superman Returns': 4.0
+    },
+    {
+      'You, Me and Dupree': 1.0
+    },
   ]
-
 }
 
 module.exports = router;
